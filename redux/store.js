f35e2b3ac181e31d51411b4c9baa7e643d126117
Belen Bahmer-Rada <belenbahmerrada@gmail.com>
2023-01-20 22:20:08 -0600

    Redux

--- redux/store.js
+++ redux/store.js
@@ -9,6 +9,6 @@ export const store = configureStore({
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
-        promotions: promotionsReducer
+        promotions: promotionsReducer,
     }
 });
